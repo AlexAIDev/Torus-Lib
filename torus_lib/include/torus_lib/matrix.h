92d3e8e4460b0b665184ea5145e12ef3db2ee392
AlexAIDev <alexaidevv@gmail.com>
2023-06-24 20:23:46 +0300

    Added determinant function

--- torus_lib/include/torus_lib/matrix.h
+++ torus_lib/include/torus_lib/matrix.h
@@ -17,14 +17,20 @@ namespace tlib
             matrix(std::initializer_list<std::initializer_list<double>>);
             ~matrix();
 
+            double trace();
+            bool is_square();
+            matrix& transpose();
+            matrix& inverse();
+            int get_size();
+
         friend std::ostream &operator<<(std::ostream &out, matrix &other)
         {
             for (int i = 0; i < other.rows; i++)
             {
-                out << "{";
+                out << "|";
                 for (int j = 0; j < other.cols; j++)
                     out << " " << other.data[i][j];
-                out << " }\n";
+                out << " |\n";
             }
             return out;
         }
@@ -54,5 +60,32 @@ namespace tlib
             matrix* new_matrix = new matrix(init);
             return *new_matrix;
         }
+
+        matrix& operator+(matrix &other)
+        {
+            if ((rows != other.rows) || (cols != other.cols))
+            {
+                printf("<!> Incorrect size of matrixes! (%d, %d) & (%d, %d) \n", rows, cols, other.rows, other.cols);
+                throw 1;
+            }
+            matrix* new_matrix = this;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                    new_matrix->data[i][j] += other.data[i][j];   
+            }
+            return *new_matrix;
+        }
+
+        std::vector<double>& operator[](int index)
+        {
+            if (index < 0 || index >= rows) 
+                throw std::out_of_range("Index out of range");
+
+            return data[index];
+        }
     };  
+
+    double determinant(matrix, int);
 } 
--- torus_lib/src/matrix.cpp
+++ torus_lib/src/matrix.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <memory>
+#include <cmath>
 #include "../include/torus_lib/matrix.h"
 
 namespace tlib
@@ -8,11 +8,19 @@ namespace tlib
     {
         this->rows = rows;
         this->cols = cols;
+
+        for (int i = 0; i < rows; i++)
+        {
+            data.push_back({});
+            for (int j = 0; j < cols; j++)
+                data[i].push_back(1);
+        }
     }
 
     matrix::matrix()
     {
         rows = cols = 0;
+        data.push_back({});
     }
 
     matrix::matrix(const matrix &other)
@@ -41,5 +49,62 @@ namespace tlib
     {
         for (int i = 0; i < rows; i++)
             data[i].clear();   
+    }
+
+    double matrix::trace()
+    {
+        int tr = 0;
+        if(is_square())
+        {
+            for (int i = 0; i < rows; i++)
+                tr += data[i][i];
+        }
+        return tr;
+    }
+
+    bool matrix::is_square()
+    {
+        return rows == cols;
+    }
+
+    int matrix::get_size()
+    {
+        if (is_square())
+            return rows;
+        return 0;
+    }
+
+    // Move to other file
+    double determinant(matrix mat, int size)
+    {
+        if (!mat.is_square())
+            throw 2;
+        if (size == 1)
+            return mat[0][0];
+        else if (size == 2)
+            return (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
+
+        int det = 0;
+        matrix submatrix(3, 3);
+
+        for (int k = 0; k < size; k++)
+        {
+            int cofactor_i = 0;
+            for (int i = 1; i < size; i++) 
+            {
+                int cofactor_j = 0;
+                for (int j = 0; j < size; j++) 
+                {
+                    if (j != k) 
+                    {
+                        submatrix[cofactor_i][cofactor_j] = mat[i][j];
+                        cofactor_j++;
+                    }
+                }
+                cofactor_i++;
+            }
+            det += (std::pow(-1, k) * mat[0][k] * determinant(submatrix, size - 1));
+        }
+        return det;
     }
 } 
